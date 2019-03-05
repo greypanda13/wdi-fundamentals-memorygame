b94917108d53e6b534ed04f076af79270460029c
Nathan Page <nathan@Nathans-MacBook-Pro.local>
2019-03-04 19:53:27 -0800

    completed basic version of memory game. could improve with scorekeeping and more rules.

--- js/main.js
+++ js/main.js
@@ -35,13 +35,31 @@ function checkForMatch() {
   }
 }
 
-function flipCard (cardId) {
+function createBoard() {
+  for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+}
+
+function flipCard () {
+  var cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
   console.log(cards[cardId].cardImage);
   console.log(cards[cardId].suit);
+
+  this.setAttribute('src', cards[cardId].cardImage);
   checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+createBoard();
+
+
+
+
+
+//when done, submit using https://goo.gl/forms/POhiqySbWZs5c0n63
